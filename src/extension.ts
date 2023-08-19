136612c3b710ddc7beffff57b1ad8bf97c3ec1a3
Long Tran <tranphilongnvd@gmail.com>
2023-08-19 11:20:23 +0700

    Get command from setting to run

--- src/extension.ts
+++ src/extension.ts
@@ -2,23 +2,18 @@ import { cwd } from "process";
 import * as vscode from "vscode";
 export function activate(context: vscode.ExtensionContext) {
     const config = vscode.workspace.getConfiguration("extensionSettings");
-    const enableFeature = config.get("enableFeature", false);
-    console.log("enableFeature:", enableFeature);
+    const command = config.get("command", "npm start");
 
     const workspaceFolders = vscode.workspace.workspaceFolders;
-    if (workspaceFolders?.length) {
-        // const firstWorkspacePath = workspaceFolders[0].uri.fsPath;
-        // const terminal = vscode.window.createTerminal({
-        //     name: "npm-start",
-        //     cwd: firstWorkspacePath,
-        // });
-        // terminal.sendText("npm start");
-        // terminal.show();
+    if (workspaceFolders?.length && command) {
+        const firstWorkspacePath = workspaceFolders[0].uri.fsPath;
+        const terminal = vscode.window.createTerminal({
+            name: "Auto command",
+            cwd: firstWorkspacePath,
+        });
+        terminal.sendText(command);
+        terminal.show();
     }
-    vscode.workspace.onDidChangeConfiguration((event) => {
-        console.log("Settings have changed:", event);
-        // Thực hiện hành động phù hợp khi cài đặt thay đổi
-    });
 }
 
 export function deactivate() {}
